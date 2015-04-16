ad4852c302d466d2f3abec4062e7e7e5378009b5
Eduardo <eduardorojas@csu.fullerton.edu>
2015-04-15 21:22:02 -0700

    Validate js and indentation

--- Assignment8/client/javascripts/app.js
+++ Assignment8/client/javascripts/app.js
@@ -1,30 +1,33 @@
 // Client-side code
 /* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
-
 var main = function() {
     "use strict";
-	var original = "",
-		newlink = "",
+    var original = "",
+        newlink = "",
         oldlink = "",
         s = "";
-	
+
     $("#submit").click(function() {
-    	original = $("#inputbox").val();
-        if(original){
-            $.post("/submit", {url: original}).done(function(res){
-                newlink = '<a href="' + original + '">' + res.shorturl +  '</a>';
-            	$("div.result").html(newlink);
-            },refreshLinks);
+        original = $("#inputbox").val();
+        if (original) {
+            $.post("/submit", {
+                url: original
+            }).done(function(res) {
+                newlink = '<a href="' + original + '">' + res.shorturl + '</a>';
+                $("div.result").html(newlink);
+            }, refreshLinks);
         }
     });
-    
+
     $("#revert").click(function() {
-    	s = $("#reverttext").val();
-        if(s){
-            $.post("/revert", {url: s}).done(function(res){
+        s = $("#reverttext").val();
+        if (s) {
+            $.post("/revert", {
+                url: s
+            }).done(function(res) {
                 console.log(res.originalurl);
-            	oldlink = '<a href="'+res.originalurl+'">'+res.originalurl+'</a>';
-            	$("div.original").html(oldlink);
+                oldlink = '<a href="' + res.originalurl + '">' + res.originalurl + '</a>';
+                $("div.original").html(oldlink);
             });
         }
     });
@@ -38,7 +41,7 @@ var main = function() {
         $.getJSON("/links", function(links) {
             $lnks.empty();
             console.log(links);
-            links.forEach(function(link){
+            links.forEach(function(link) {
                 $lnks.append($("<p>").html(link.shorturl + "    |   Click count: " + link.clicks));
             });
         });
--- Assignment8/server.js
+++ Assignment8/server.js
@@ -14,68 +14,86 @@ mongoose.connect("mongodb://localhost/urls");
 //This code was copied from the Mongoose documentation
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function (callback) {
-  // yay!
+db.once("open", function(callback) {
+    // yay!
 });
 
 var linkSchema = new mongoose.Schema({
     longurl: String,
     shorturl: String,
-    clicks: {type: Number, default: 0}
+    clicks: {
+        type: Number,
+        default: 0
+    }
 });
 
-var Url = mongoose.model("Url",linkSchema);
+var Url = mongoose.model("Url", linkSchema);
 
 app.use(express.static(__dirname + "/client"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Create our Express-powered HTTP server
 http.createServer(app).listen(3000);
 
 
-app.get("/links", function(req,res) {
-    Url.find({}).sort({clicks: "desc"}).limit(10).exec(function(err, urls){
-        if (err){
+app.get("/links", function(req, res) {
+    Url.find({}).sort({
+        clicks: "desc"
+    }).limit(10).exec(function(err, urls) {
+        if (err) {
             return console.error(err);
         }
         res.json(urls);
     });
 });
 
-app.post("/submit", function (req, res) {
+app.post("/submit", function(req, res) {
     sh = "http://localhost:3000/" + shortid.generate();
-	
-    var newlink = new Url({longurl : req.body.url, shorturl: sh});
+
+    var newlink = new Url({
+        longurl: req.body.url,
+        shorturl: sh
+    });
     newlink.save(function(err) {
-        if (err){
+        if (err) {
             return console.error(err);
         }
     });
-	res.json({shorturl: sh});
+    res.json({
+        shorturl: sh
+    });
 });
 
-app.post("/revert", function (req, res) {
-    Url.findOne({shorturl : req.body.url},function(err,url){
-        if (err){
+app.post("/revert", function(req, res) {
+    Url.findOne({
+        shorturl: req.body.url
+    }, function(err, url) {
+        if (err) {
             return console.error(err);
         }
-        res.json({originalurl: url.longurl});
+        res.json({
+            originalurl: url.longurl
+        });
     });
 
 });
 
-app.get("/:id", function (req, res) {
+app.get("/:id", function(req, res) {
     var sh = "http://localhost:3000/" + req.params.id.trim();
 
-    Url.findOne({shorturl : sh},function(err,url){
+    Url.findOne({
+        shorturl: sh
+    }, function(err, url) {
         url.clicks += 1;
-        url.save(function(err){
-            if(err){
+        url.save(function(err) {
+            if (err) {
                 return console.error(err);
             }
             res.redirect(url.longurl);
-        }); 
+        });
     });
 
 });
